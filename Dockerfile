3a43fbcb1b849f996b5fe7e62da5c368a8912c44
Jeremy Gresham <jsg57@duke.edu>
2021-10-19 17:37:29 -0400

    pushing current version. Still an error in docker build

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,14 @@ ARG DEBIAN_FRONTEND=noninteractive
 
 COPY . /TASE_BUILD/
 
-RUN apt-get update -y && apt-get install --no-install-recommends -y make git software-properties-common binutils emacs bison flex libboost-program-options-dev perl zlib1g-dev libcap-dev libncurses-dev && \
-cd /TASE_BUILD/test/ && NCPU=8 ./provision.sh && \
-cd /TASE_BUILD/install/ &&  make setup && \
+RUN apt-get update -y && apt-get install --no-install-recommends -y build-essential binutils bison flex make cmake git wget curl bash-completion software-properties-common linux-tools-generic linux-tools-common python3-dev python3-pip zip unzip emacs libboost-program-options-dev perl zlib1g-dev libcap-dev libncurses5 libncurses-dev && \
+mkdir -p /TASE/llvm-3.4.2 && \
+curl -s https://releases.llvm.org/3.4.2/clang+llvm-3.4.2-x86_64-linux-gnu-ubuntu-14.04.xz | tar xJvf - -C /TASE/llvm-3.4.2 --strip 1 && \
+mkdir -p /TASE/include/ /TASE/scripts/ && \
+cp -r /TASE_BUILD/test/tase/include/tase/ /TASE/include/tase/ && \
+cp -r /TASE_BUILD/test/other/ /TASE/include/traps/ && \
+cp -r /TASE_BUILD/openssl/include/ /TASE/include/openssl/ && \
+cp /TASE_BUILD/test/tase/tase_link.ld /TASE/ && \
+cp -r /TASE_BUILD/test/scripts/ /TASE/scripts/ #&& \
+cd /TASE_BUILD/install/ &&  make -j 12 setup && \
 rm -rf /TASE_BUILD/ /var/lib/apt/lists/* && apt-get autoremove
--- install/localize.sh
+++ install/localize.sh
@@ -1,34 +1,36 @@
 set -e
 
+OBJ="$1"
+
 cat libtasec.syms | while read LIB
 do
-    objcopy --localize-symbol=$LIB musl.o
+    objcopy --localize-symbol=$LIB $OBJ
 done
 
 #These are some symbol redefinition hacks that help us link in much of
 #libc from musl.  Specifically, the a_ctz_64 and a_clz_64 functions have
 #bsr instructions when they're compiled in TASE, so we just trap on those for now
 #and use a model in KLEE.
-objcopy --redefine-syms=/TASE_BUILD/test/scripts/redefinedSF musl.o
-objcopy --redefine-syms=/TASE_BUILD/test/scripts/redefinedStrConv musl.o
+objcopy --redefine-syms=/TASE_BUILD/test/scripts/redefinedSF $OBJ
+objcopy --redefine-syms=/TASE_BUILD/test/scripts/redefinedStrConv $OBJ
 
-objcopy --redefine-sym sprintf=sprintf_tase musl.o
-objcopy --redefine-sym printf=printf_tase musl.o
-objcopy --redefine-sym puts=puts_tase_shim musl.o
-objcopy --globalize-symbol=a_ctz_64 musl.o
-objcopy --globalize-symbol=a_clz_64 musl.o
-objcopy --redefine-sym a_ctz_64=a_ctz_64_tase musl.o
-objcopy --redefine-sym a_clz_64=a_clz_64_tase musl.o
+objcopy --redefine-sym sprintf=sprintf_tase $OBJ
+objcopy --redefine-sym printf=printf_tase $OBJ
+objcopy --redefine-sym puts=puts_tase_shim $OBJ
+objcopy --globalize-symbol=a_ctz_64 $OBJ
+objcopy --globalize-symbol=a_clz_64 $OBJ
+objcopy --redefine-sym a_ctz_64=a_ctz_64_tase $OBJ
+objcopy --redefine-sym a_clz_64=a_clz_64_tase $OBJ
 
-objcopy --redefine-sym calloc=calloc_tase_shim musl.o
-objcopy --redefine-sym realloc=realloc_tase_shim musl.o
-objcopy --redefine-sym malloc=malloc_tase_shim musl.o
-objcopy --redefine-sym free=free_tase_shim musl.o
-objcopy --redefine-sym getc_unlocked=getc_unlocked_tase_shim musl.o
+objcopy --redefine-sym calloc=calloc_tase_shim $OBJ
+objcopy --redefine-sym realloc=realloc_tase_shim $OBJ
+objcopy --redefine-sym malloc=malloc_tase_shim $OBJ
+objcopy --redefine-sym free=free_tase_shim $OBJ
+objcopy --redefine-sym getc_unlocked=getc_unlocked_tase_shim $OBJ
 
 #Changed because we now link in the definition of memcpy from musl, but still
 #want to trap for efficiency.
-objcopy --redefine-sym memcpy=memcpy_tase musl.o
-objcopy --globalize-symbol=memcpy_tase musl.o
+objcopy --redefine-sym memcpy=memcpy_tase $OBJ
+objcopy --globalize-symbol=memcpy_tase $OBJ
 
-objcopy --redefine-sym main=tase_project_entry musl.o
+objcopy --redefine-sym main=tase_project_entry $OBJ
