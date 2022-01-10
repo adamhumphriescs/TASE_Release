d7353a7022673a956472861811bc2fbc6810c708
Jeremy Gresham <jsg57@duke.edu>
2022-01-10 11:59:25 -0500

    removing memcpy_tase from shims

--- test/tase/traps/tase_shims.c
+++ test/tase/traps/tase_shims.c
@@ -43,9 +43,9 @@ void free_tase_shim(void * ptr) {
   return;
 }
 
-void * memcpy_tase(void * dest, const void * src, unsigned long n) {
+/*void * memcpy_tase(void * dest, const void * src, unsigned long n) {
   return 0LL;
-}
+}*/
 void * memcpy_tase_shim(void * dest, const void * src, unsigned long n) {
   return memcpy_tase(dest,src,n);
 }
