0b19c06c951bbccb64b3c70c5c27eea02b37ad3f
Jeremy Gresham <jsg57@duke.edu>
2022-10-24 21:42:33 -0400

    fixing runtest log issue

--- microbenchmarks/slidingscale/bigNum/runtest.sh
+++ microbenchmarks/slidingscale/bigNum/runtest.sh
@@ -10,7 +10,7 @@ do
 
     for i in {0..50000..1000}
     do
-	        echo $i, `   cd build/ && (time( ./run.sh -optimizeConstMemOps=TRUE -use-forked-solver=FALSE -rewrite-equalities=FALSE -use-independent-solver=TRUE   -use-cex-cache=TRUE -use-cache=FALSE -retryMax=1 -tranBBMax=16 -modelDebug=FALSE -useCMS4=FALSE  -use-legacy-independent-solver=TRUE -UseCanonicalization=TRUE -check-overshift=FALSE -singleStepping -noLog=TRUE - $i 50000 1 >  /dev/null 2>&1) ) |&  grep user | cut -d$'\t' -f2 `, 0 >> SlidingScaleResults.txt 2>&1
+	        echo $i, `   cd build/ && (time( ./run.sh -optimizeConstMemOps=TRUE -use-forked-solver=FALSE -rewrite-equalities=FALSE -use-independent-solver=TRUE   -use-cex-cache=TRUE -use-cache=FALSE -retryMax=1 -tranBBMax=16 -modelDebug=FALSE -useCMS4=FALSE  -use-legacy-independent-solver=TRUE -UseCanonicalization=TRUE -check-overshift=FALSE -singleStepping -log=false - $i 50000 1 >  /dev/null 2>&1) ) |&  grep user | cut -d$'\t' -f2 `, 0 >> SlidingScaleResults.txt 2>&1
 
 
     done
