336b29356626599203c1e75a1b254379db3020a1
Jeremy Gresham <jsg57@duke.edu>
2023-01-31 09:19:48 -0500

    minor updates

--- microbenchmarks/slidingscale/bigNum/harness.c
+++ microbenchmarks/slidingscale/bigNum/harness.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "tasetraps.h"
 
+extern void make_byte_symbolic(void* addr);
+
 /*void __attribute__((noinline)) make_byte_symbolic(void* addr) {
   printf("Should never reach this line! Should've marked 0x%lx symbolic \n", (uint64_t) addr);
   return;
